bef19bad34592463cdaf8d74e9d478ec1d641d61
jmatias-2019152 <jmatias-2019152@kinal.edu.gt>
2024-03-02 22:18:51 -0600

    Correcion

--- src/helpers/db-validators.js
+++
@@ -1,8 +0,0 @@
-import Usuario from '../usuario/usuario.model.js'
-
-export const existeEmail = async (correo = '') => {
-    const existeEmail = await Usuario.findOne({ correo });
-    if (existeEmail) {
-        throw new Error(`El email ${correo} ya fue registrado`);
-    }
-}
